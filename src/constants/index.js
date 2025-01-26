cc3b7a619f2fa6c8d5da5dc2f565a360122e827c
Madannadar <154582270+Madannadar@users.noreply.github.com>
2025-01-26 18:59:38 +0530

    project complete

---
+++ src/constants/index.js
@@ -0,0 +1,297 @@
+export const navLinks = [
+    {
+      id: 1,
+      name: 'Home',
+      href: '#home',
+    },
+    {
+      id: 2,
+      name: 'About',
+      href: '#about',
+    },
+    {
+      id: 3,
+      name: 'Work',
+      href: '#work',
+    },
+    {
+      id: 4,
+      name: 'Contact',
+      href: '#contact',
+    },
+  ];
+  
+  export const clientReviews = [
+    {
+      id: 1,
+      name: 'Emily Johnson',
+      position: 'Marketing Director at GreenLeaf',
+      img: 'assets/review1.png',
+      review:
+        'Working with Adrian was a fantastic experience. He transformed our outdated website into a modern, user-friendly platform. His attention to detail and commitment to quality are unmatched. Highly recommend him for any web dev projects.',
+    },
+    {
+      id: 2,
+      name: 'Mark Rogers',
+      position: 'Founder of TechGear Shop',
+      img: 'assets/review2.png',
+      review:
+        'Adrian’s expertise in web development is truly impressive. He delivered a robust and scalable solution for our e-commerce site, and our online sales have significantly increased since the launch. He’s a true professional! Fantastic work.',
+    },
+    {
+      id: 3,
+      name: 'John Dohsas',
+      position: 'Project Manager at UrbanTech ',
+      img: 'assets/review3.png',
+      review:
+        'I can’t say enough good things about Adrian. He was able to take our complex project requirements and turn them into a seamless, functional website. His problem-solving abilities are outstanding.',
+    },
+    {
+      id: 4,
+      name: 'Ether Smith',
+      position: 'CEO of BrightStar Enterprises',
+      img: 'assets/review4.png',
+      review:
+        'Adrian was a pleasure to work with. He understood our requirements perfectly and delivered a website that exceeded our expectations. His skills in both frontend backend dev are top-notch.',
+    },
+  ];
+  
+  export const myProjects = [
+    {
+      title: 'SIES Placement List Display',
+      desc: 'SIES placement list / SIES T&P Cell, is a project entrusted by one of my seniors who is a member of the placement cell and wanted to build a better site to show all the placed studenets to the world.',
+      subdesc:
+        'Build a Responsive JS project that uses Mongoo db for database, EXPRESS acting as the bridge between frontend and database, UUID to generate unique identifiers without requiring a centralized authority in my case it was used to give UUIDs for file upload, BCRYPT for hashing the password before saving it to the database, AXIOS to make http request from browser to server, JSW for token generater and MULTER for storing the image locally ',
+      href: 'https://github.com/Madannadar/clg_placement_project.git',
+      texture: '/textures/project/sies_placement_video.mp4',
+      logo: '/assets/project-logo1.png',
+      logoStyle: {
+        backgroundColor: '#2A1816',
+        border: '0.2px solid #36201D',
+        boxShadow: '0px 0px 60px 0px #AA3C304D',
+      },
+      spotlight: '/assets/spotlight1.png',
+      tags: [
+        {
+          id: 1,
+          name: 'JavaScript',
+          path: '/assets/JavaScript-Symbol.png',
+        },
+        {
+          id: 2,
+          name: 'TailwindCSS',
+          path: 'assets/tailwindcss.png',
+        },
+        {
+          id: 3,
+          name: 'TypeScript',
+          path: '/assets/typescript.png',
+        },
+        {
+          id: 4,
+          name: 'Framer Motion',
+          path: '/assets/framer.png',
+        },
+      ],
+    },
+    {
+      title: 'Pintrest Clone',
+      desc: 'Pintrest clone is a project that i wanted to make just because of its complex structure, features, and file handeling',
+      subdesc:
+        'I built the project using React for the front-end to create an interactive and dynamic user interface. React’s component-based architecture made it easy to structure the app and handle state management, enabling smooth user interactions. This is especially important for a visually-driven platform like Pinterest, where efficient rendering and responsiveness are key to providing a great user experience.',
+      href: 'https://pinterest-clone-od14.onrender.com',
+      texture: '/textures/project/pintrestVideo.mp4',
+      logo: '/assets/project-logo2.png',
+      logoStyle: {
+        backgroundColor: '#13202F',
+        border: '0.2px solid #17293E',
+        boxShadow: '0px 0px 60px 0px #2F6DB54D',
+      },
+      spotlight: '/assets/spotlight2.png',
+      tags: [
+        {
+          id: 1,
+          name: 'React.js',
+          path: '/assets/react.svg',
+        },
+        {
+          id: 2,
+          name: 'TailwindCSS',
+          path: 'assets/tailwindcss.png',
+        },
+        {
+          id: 3,
+          name: 'TypeScript',
+          path: '/assets/typescript.png',
+        },
+        {
+          id: 4,
+          name: 'Framer Motion',
+          path: '/assets/framer.png',
+        },
+      ],
+    },
+    {
+      title: 'Youtube Frontend Using React',
+      desc: 'An frontend project that uses youtubes api for fetching data and showing them, as this project was simple in making i added new feature in the project',
+      subdesc:
+        'I created a YouTube frontend site using React to replicate the core features of the popular video-sharing platform. By leveraging React component-based architecture, I was able to design a dynamic and responsive UI that mimics the look and feel of YouTube, including elements like video thumbnails, search functionality, and video details. React efficient state management allows for seamless user interaction, while components are easily reusable, making the app highly maintainable and scalable. This project provides a solid foundation for any further development of a video-sharing platform or integration with APIs to fetch real-time content.',
+      href: 'https://www.youtube.com/watch?v=lEflo_sc82g',
+      texture: '/textures/project/project3.mp4',
+      logo: '/assets/project-logo3.png',
+      logoStyle: {
+        backgroundColor: '#60f5a1',
+        background:
+          'linear-gradient(0deg, #60F5A150, #60F5A150), linear-gradient(180deg, rgba(255, 255, 255, 0.9) 0%, rgba(208, 213, 221, 0.8) 100%)',
+        border: '0.2px solid rgba(208, 213, 221, 1)',
+        boxShadow: '0px 0px 60px 0px rgba(35, 131, 96, 0.3)',
+      },
+      spotlight: '/assets/spotlight3.png',
+      tags: [
+        {
+          id: 1,
+          name: 'React.js',
+          path: '/assets/react.svg',
+        },
+        {
+          id: 2,
+          name: 'TailwindCSS',
+          path: 'assets/tailwindcss.png',
+        },
+        {
+          id: 3,
+          name: 'TypeScript',
+          path: '/assets/typescript.png',
+        },
+        {
+          id: 4,
+          name: 'Framer Motion',
+          path: '/assets/framer.png',
+        },
+      ],
+    },
+    {
+      title: 'FieldRental',
+      desc: 'Horizon is a comprehensive online banking platform that offers users a centralized finance management dashboard. It allows users to connect multiple bank accounts, monitor real-time transactions, and seamlessly transfer money to other users.',
+      subdesc:
+        'Built with Next.js 14 Appwrite, Dwolla and Plaid, Horizon ensures a smooth and secure banking experience, tailored to meet the needs of modern consumers.',
+      href: 'https://www.youtube.com/watch?v=PuOVqP_cjkE',
+      texture: '/textures/project/project4.mp4',
+      logo: '/assets/project-logo4.png',
+      logoStyle: {
+        backgroundColor: '#0E1F38',
+        border: '0.2px solid #0E2D58',
+        boxShadow: '0px 0px 60px 0px #2F67B64D',
+      },
+      spotlight: '/assets/spotlight4.png',
+      tags: [
+        {
+          id: 1,
+          name: 'React.js',
+          path: '/assets/react.svg',
+        },
+        {
+          id: 2,
+          name: 'TailwindCSS',
+          path: 'assets/tailwindcss.png',
+        },
+        {
+          id: 3,
+          name: 'TypeScript',
+          path: '/assets/typescript.png',
+        },
+        {
+          id: 4,
+          name: 'Framer Motion',
+          path: '/assets/framer.png',
+        },
+      ],
+    },
+    {
+      title: 'Imaginify - AI Photo Manipulation App',
+      desc: 'Imaginify is a groundbreaking Software-as-a-Service application that empowers users to create stunning photo manipulations using AI technology. With features like AI-driven image editing, a payments system, and a credits-based model.',
+      subdesc:
+        'Built with Next.js 14, Cloudinary AI, Clerk, and Stripe, Imaginify combines cutting-edge technology with a user-centric approach. It can be turned into a side income or even a full-fledged business.',
+      href: 'https://www.youtube.com/watch?v=Ahwoks_dawU',
+      texture: '/textures/project/project5.mp4',
+      logo: '/assets/project-logo5.png',
+      logoStyle: {
+        backgroundColor: '#1C1A43',
+        border: '0.2px solid #252262',
+        boxShadow: '0px 0px 60px 0px #635BFF4D',
+      },
+      spotlight: '/assets/spotlight5.png',
+      tags: [
+        {
+          id: 1,
+          name: 'React.js',
+          path: '/assets/react.svg',
+        },
+        {
+          id: 2,
+          name: 'TailwindCSS',
+          path: 'assets/tailwindcss.png',
+        },
+        {
+          id: 3,
+          name: 'TypeScript',
+          path: '/assets/typescript.png',
+        },
+        {
+          id: 4,
+          name: 'Framer Motion',
+          path: '/assets/framer.png',
+        },
+      ],
+    },
+  ];
+  
+  export const calculateSizes = (isSmall, isMobile, isTablet) => {
+    return {
+      deskScale: isSmall ? 0.05 : isMobile ? 0.06 : 0.065,
+      deskPosition: isMobile ? [0.5, -4.5, 0] : [0.25, -5.5, 0],
+      cubePosition: isSmall ? [3, -6.5, 0] : isMobile ? [5, -5, 0] : isTablet ? [5, -5, 0] : [9, -5.5, 0],
+      reactLogoPosition: isSmall ? [3, 4, 0] : isMobile ? [5, 4, 0] : isTablet ? [5, 4, 0] : [12, 3, 0],
+      ringPosition: isSmall ? [-5, 7, 0] : isMobile ? [-10, 10, 0] : isTablet ? [-12, 10, 0] : [-24, 10, 0],
+      targetPosition: isSmall ? [-5, -10, -10] : isMobile ? [-9, -10, -10] : isTablet ? [-11, -7, -10] : [-11, -9, -10],
+    };
+  };
+  
+  export const workExperiences = [
+    {
+      id: 1,
+      name: 'FieldRentals',
+      pos: 'Team member',
+      duration: 'Sep 2024 - Oct 2024',
+      title: "A React project that solves the problem of farmers who do not have the resources to buy their own website.Also won second price at project presentation competition Cognition",
+      icon: '/assets/framer.svg',
+      animation: 'victory',
+    },
+    {
+      id: 2,
+      name: 'Sies Placement List project',
+      pos: 'Lead Web Developer',
+      duration: 'Nov 2024 - Jan 2024',
+      title: "SIES placement list / SIES T&P Cell, is a project entrusted by one of my seniors who is a member of the placement cell and wanted to build a better site to show all the placed studenets to the world.",
+      icon: '/assets/figma.svg',
+      animation: 'clapping',
+    },
+    {
+      id: 3,
+      name: 'Youtube Frontend Site',
+      pos: 'Solo project',
+      duration: 'Dec 2024 - Jan 2025',
+      title: "I build this project for its api handeling and for frontend and i added some features on it.",
+      icon: '/assets/notion.svg',
+      animation: 'victory',
+    },
+    {
+      id: 4,
+      name: 'Pintrest Clone',
+      pos: 'Solo project',
+      duration: 'Dec 2024 - present',
+      title: "Pintrest clone is a project that i wanted to make just because of its complex structure, features, and file handeling.",
+      icon: '/assets/notion.svg',
+      animation: 'salute',
+    },
+  ];
